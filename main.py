23804dfdebc4b2d68e0ad1dda15ae961c328cb7f
soft-trans <it@transpipeintegrity.com>
2024-08-08 14:58:25 +0530

    adding 4th lineas

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def get_last_data():
         "1st object added 2024-08-08 14:12",
         "2st object added 2024-08-08 14:44",
         "pushing directly from ui from pycharm",
-        "My Line 4 - Arth"a
+        "My Line 4 - Arth"
     ]
 
     return lst[-1]

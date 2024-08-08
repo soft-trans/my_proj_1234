702ef9cb3094de3606c3126c49f57664715a66ad
soft-trans <it@transpipeintegrity.com>
2024-08-08 14:47:05 +0530

    adding third line in list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def get_last_data():
     lst = [
         "1st object added 2024-08-08 14:12",
         "2st object added 2024-08-08 14:44",
+        "pushing directly from ui from pycharm",
     ]
 
     return lst[-1]
